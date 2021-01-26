0d181da0ddaeda23bec7d228e7100e475fec907b
Davide Bettio <davide@uninstall.it>
2021-01-26 19:03:46 +0100

    Prepare v0.11.4
    Bump version numbers to 0.11.4 and update CHANGELOG.md.
    
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- test/utils/astarte_resource.go
+++ test/utils/astarte_resource.go
@@ -33,7 +33,7 @@ var AstarteTestResource *operator.Astarte = &operator.Astarte{
 		Name: "example-astarte",
 	},
 	Spec: operator.AstarteSpec{
-		Version: "0.11.3",
+		Version: "0.11.4",
 		// Use the "Recreate" strategy. Some test environments are really constrained, and might not have enough
 		// resources to support RollingUpdate.
 		DeploymentStrategy: &appsv1.DeploymentStrategy{
