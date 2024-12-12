9f6a1c8ed8860306bc83b62d125839c62c569dfc
Mattia Mazzucato <mattia.mazzucato@secomind.com>
2024-12-12 17:31:44 +0100

    Scaffold the v1alpha2 api for Flow
    operator-sdk create api --group api --version v1alpha2 --kind Flow
    
    Signed-off-by: Mattia Mazzucato <mattia.mazzucato@secomind.com>

--- cmd/main.go
+++ cmd/main.go
@@ -151,6 +151,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Astarte")
 		os.Exit(1)
 	}
+	if err = (&controller.FlowReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Flow")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
