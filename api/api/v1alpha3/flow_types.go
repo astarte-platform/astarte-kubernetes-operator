e7d1020f73391ed5aab218305637f17e83f619ec
Mattia Mazzucato <mattia.mazzucato@secomind.com>
2024-12-13 11:48:37 +0100

    Scaffold the v1alpha3 api for Flow
    operator-sdk create api --group api --version v1alpha3 --kind Flow
    
    Signed-off-by: Mattia Mazzucato <mattia.mazzucato@secomind.com>

---
+++ api/api/v1alpha3/flow_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// FlowSpec defines the desired state of Flow
+type FlowSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Flow. Edit flow_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// FlowStatus defines the observed state of Flow
+type FlowStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Flow is the Schema for the flows API
+type Flow struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   FlowSpec   `json:"spec,omitempty"`
+	Status FlowStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// FlowList contains a list of Flow
+type FlowList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Flow `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Flow{}, &FlowList{})
+}
