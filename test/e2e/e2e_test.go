57edfb7df1305e7647f9f0f4de9213d304a8eb28
Luca Marchiori <luca.marchiori@secomind.com>
2025-08-12 15:31:40 +0200

    feat: target astarte 1.3 manifests in e2e tests
    Signed-off-by: Luca Marchiori <luca.marchiori@secomind.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -143,8 +143,7 @@ var _ = Describe("controller", Ordered, func() {
 			EventuallyWithOffset(1, verifyControllerUp, utils.DefaultTimeout, utils.DefaultRetryInterval).Should(Succeed())
 
 			targetTestVersions := map[string]string{
-				"1.1": filepath.Join(projectDir, "/test/manifests/api_v1alpha2_astarte_1.1.yaml"),
-				"1.2": filepath.Join(projectDir, "/test/manifests/api_v1alpha2_astarte_1.2.yaml"),
+				"1.3": filepath.Join(projectDir, "/test/manifests/api_v2alpha1_astarte_1.3.yaml"),
 			}
 
 			for k, v := range targetTestVersions {
