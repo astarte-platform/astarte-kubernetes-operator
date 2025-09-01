c2bd577ecf70d55e7894dd8e75ea826695a0e6ac
Luca Marchiori <luca.marchiori@secomind.com>
2025-09-01 13:17:43 +0200

    ci: add CI to execute unit/integration tests on GH and clean up failing suite test for the next dev phase
    Signed-off-by: Luca Marchiori <luca.marchiori@secomind.com>

--- internal/controller/api/suite_test.go
+++ internal/controller/api/suite_test.go
@@ -56,7 +56,7 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
 
 		// The BinaryAssetsDirectory is only required if you want to run the tests directly
@@ -64,7 +64,7 @@ var _ = BeforeSuite(func() {
 		// default path defined in controller-runtime which is /usr/local/kubebuilder/.
 		// Note that you must have the required binaries setup under the bin directory to perform
 		// the tests directly. When we run make test it will be setup and used automatically.
-		BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s",
+		BinaryAssetsDirectory: filepath.Join("..", "..", "..", "bin", "k8s",
 			fmt.Sprintf("1.30.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
 	}
 
